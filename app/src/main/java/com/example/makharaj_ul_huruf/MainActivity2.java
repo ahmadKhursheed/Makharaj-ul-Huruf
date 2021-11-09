cc6d8dcd0bc66391cb4debdfd366012f050b81ff
ahmadkhursheed <512ahmadkhursheed@gmail.com>
2021-11-09 13:26:46 +0500

    Added buttons in activity2

--- app/src/main/java/com/example/makharaj_ul_huruf/MainActivity2.java
+++ app/src/main/java/com/example/makharaj_ul_huruf/MainActivity2.java
@@ -3,12 +3,25 @@ package com.example.makharaj_ul_huruf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        btn1=(Button)findViewById(R.id.button1);
+        btn2=(Button)findViewById(R.id.button7);
+        btn3=(Button)findViewById(R.id.button3);
+        btn4=(Button)findViewById(R.id.button4);
+        btn5=(Button)findViewById(R.id.button5);
+        btn6=(Button)findViewById(R.id.button6);
+        btn7=(Button)findViewById(R.id.button7);
     }
 }
