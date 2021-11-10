c9a113b825c4b6600b6728b67764023e052f910d
ahmadkhursheed <512ahmadkhursheed@gmail.com>
2021-11-10 23:42:42 +0500

    set images in activities

--- app/src/main/java/com/example/makharaj_ul_huruf/MainActivity4.java
+++ app/src/main/java/com/example/makharaj_ul_huruf/MainActivity4.java
@@ -9,8 +9,8 @@ import android.widget.TextView;
 public class MainActivity4 extends AppCompatActivity {
 
 
-    public TextView head;
-    public ImageView img1,img2;
+    TextView head;
+    ImageView img1,img2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +18,39 @@ public class MainActivity4 extends AppCompatActivity {
         setContentView(R.layout.activity_main4);
 
         String BtnName = getIntent().getStringExtra("button");
-        img1=findViewById(R.id.imageView);
+        img1=findViewById(R.id.imageView2);
+        img2=findViewById(R.id.imageView3);
         head=findViewById(R.id.textView);
         head.setText(BtnName.toString());
 
 
-//        if (BtnName.equals("Halqiyah")) {
-//            img1.setBackgroundResource(R.drawable.halqiyah);
-//
-//        }
+        if (BtnName.equals("Halqiyah")) {
+            img1.setBackgroundResource(R.drawable.halqiyah1);
+            img2.setBackgroundResource(R.drawable.halqiyah);
+        }
+        else if (BtnName.equals("Lahatiyah")) {
+            img1.setBackgroundResource(R.drawable.lahatiyah1);
+            img2.setBackgroundResource(R.drawable.lahatiyah);
+        }
+        else if (BtnName.equals("Shajariyah-Haafiyah")) {
+            img1.setBackgroundResource(R.drawable.shajar1);
+            img2.setBackgroundResource(R.drawable.shajar);
+        }
+        else if (BtnName.equals("Tarfiyah")) {
+            img1.setBackgroundResource(R.drawable.tarfiyah1);
+            img2.setBackgroundResource(R.drawable.tarfiyah);
+        }
+        else if (BtnName.equals("Nit-eeyah")) {
+            img1.setBackgroundResource(R.drawable.nit1);
+            img2.setBackgroundResource(R.drawable.nit);
+        }
+        else if (BtnName.equals("Lisaveyah")) {
+            img1.setBackgroundResource(R.drawable.lahatiyah1);
+            img2.setBackgroundResource(R.drawable.lahatiyah);
+        }
+        else{
+            img1.setBackgroundResource(R.drawable.ghunna2);
+            img2.setBackgroundResource(R.drawable.ghunna1);
+        }
     }
 }
